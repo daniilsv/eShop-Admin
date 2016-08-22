940a74b789a77da42d3428f56799debe4a1c3b1f
gicha <01gicha01@gmail.com>
2016-08-22 13:11:16 +0300

    Add PaymentTypes

--- app/src/main/java/ru/dvs/eshop/admin/data/components/eshop/PaymentTypes.java
+++ app/src/main/java/ru/dvs/eshop/admin/data/components/eshop/PaymentTypes.java
@@ -1,11 +1,16 @@
 package ru.dvs.eshop.admin.data.components.eshop;
 
+/**
+ * Created by MSI1 on 22.08.2016.
+ */
+
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import ru.dvs.eshop.admin.data.DB;
@@ -13,18 +18,21 @@ import ru.dvs.eshop.admin.data.components.Model;
 import ru.dvs.eshop.admin.data.network.POSTQuery;
 
 /**
- * Производитель
+ * Виды оплаты
  */
-public class Vendor extends Model {
+public class PaymentTypes extends Model {
     int original_id;
     int id;
     boolean is_enabled;
+    int ordering; //Порядок вывода(сортировка)
+    String name;
     String title;
+    String description;
     //Иконки - original, big, small
     HashMap<String, String> icons_href; //Ссылки на иконки
     HashMap<String, Drawable> icons; //Сами иконки в памяти устройства
-    String description;
-    int ordering; //Порядок вывода(сортировка)
+    ArrayList options;
+
 
     public void loadFromSite() {
         POSTQuery task = new POSTQuery(site.host, site.token) {
@@ -39,24 +47,26 @@ public class Vendor extends Model {
                 try {
                     JSONObject node = new JSONObject(response);
 
-//TODO: Ask DVS for response syntax number 0
+//TODO: Ask DVS for response syntax number 1
 
 //NOTE: foreach items as item {
                     int orig_id = item.getInt("original_id");
                     HashMap<String, String> map = new HashMap<>();
                     map.put("original_id", "");
                     map.put("is_enabled", "");
+                    map.put("name", "");
                     map.put("title", "");
                     map.put("icon", "");
                     map.put("description", "");
                     map.put("url", "");
                     map.put("ordering", "");
+                    map.put("options", "");
                     //Проверяем таблицу на наличие элемента с таким original_id
-                    Cursor bd_item = DB.query("com_eshop_vendors", null, null, null, null, "original_id=" + orig_id, null);
+                    Cursor bd_item = DB.query("com_eshop_payment_types", null, null, null, null, "original_id=" + orig_id, null);
                     if (bd_item != null && bd_item.moveToFirst()) {//Элемент в таблице есть - обновляем данные
-                        DB.update("com_eshop_vendors", bd_item.getInt(bd_item.getColumnIndex("id")), map);
+                        DB.update("com_eshop_payment_types", bd_item.getInt(bd_item.getColumnIndex("id")), map);
                     } else {//Элемента в таблиц нет - добавляем его
-                        DB.insert("com_eshop_vendors", map);
+                        DB.insert("com_eshop_payment_types", map);
                     }
 //NOTE: }
                 } catch (JSONException e) {
@@ -66,7 +76,7 @@ public class Vendor extends Model {
             }
         };
         task.put("controller", "eshop");
-        task.put("method", "get_vendors");
+        task.put("method", "get_payment_types");
         task.execute();
     }
 
