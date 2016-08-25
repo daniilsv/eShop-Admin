aead58b1c735b66216ce8376cedf346881508143
gicha <01gicha01@gmail.com>
2016-08-25 21:23:24 +0300

    Merge branch 'master' of D:\eShop-Admin with conflicts.

--- app/src/main/java/ru/dvs/eshop/admin/data/network/FILEQuery.java
+++ app/src/main/java/ru/dvs/eshop/admin/data/network/FILEQuery.java
@@ -22,21 +22,16 @@ public class FILEQuery extends AsyncTask<Void, Void, Void> {
 
     public FILEQuery(String _url, String destination) {
         mUrl = _url;
-        String r = "";
-        String t[] = destination.split("/");
-
-        for (int i = 0; i < t.length - 1; i++) {
-            r += t[i] + "/";
-        }
-        new File(r).mkdirs();
         result = new File(destination);
     }
 
+
     @Override
-    protected Void doInBackground(Void... params) {
-        if (!Utils.hasConnection(Core.getInstance().context))
-            return null;
+    public Void doInBackground(Void... params) {
         try {
+            if (!Utils.hasConnection(Core.getInstance().context)) {
+                return null;
+            }
             mUrl = (mUrl.contains("https://") ? "https://" : "http://") + mUrl.replaceAll("\\s+|http://|https://", "");
             BufferedInputStream in = new BufferedInputStream(new URL(mUrl).openStream());
             FileOutputStream out = new FileOutputStream(result);
@@ -50,4 +45,9 @@ public class FILEQuery extends AsyncTask<Void, Void, Void> {
         }
         return null;
     }
+
+
+    //if (Utils.hasConnection())
+
+
 }
