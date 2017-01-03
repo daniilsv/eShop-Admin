e00ff4442b040feb4201e9a7331271ba468f8b47
MysticalNobody <micronanobot.ver3@gmail.com>
2017-01-03 19:38:01 +0400

    add tooltip+set title as requed field

--- app/src/main/java/ru/dvs/eshop/admin/ui/fragments/ItemEditFragment.java
+++ app/src/main/java/ru/dvs/eshop/admin/ui/fragments/ItemEditFragment.java
@@ -4,13 +4,17 @@ import android.animation.ObjectAnimator;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -28,6 +32,7 @@ public class ItemEditFragment extends Fragment {
     ViewGroup insertPointView;
     View fragment_view;
     Model item;
+    boolean enableSave=false;
     AlertDialog.Builder ad;
     private boolean mIsAdding = false;
     private ItemViewFragment mitemViewFragment;
@@ -87,12 +92,38 @@ public class ItemEditFragment extends Fragment {
         insertPointView = (ViewGroup) fragment_view.findViewById(R.id.item_frame);
 
         fillData();
-        Button editBut = (Button) fragment_view.findViewById(R.id.button_save);
+        final Button editBut = (Button) fragment_view.findViewById(R.id.button_save);
+        editBut.setTextColor(ContextCompat.getColor(getActivity(), R.color.cardview_dark_background));
+        EditText edText = (EditText) fragment_view.findViewById(R.id.edit_vendor_title);
+        edText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(count!=0){
+                    editBut.setTextColor(ContextCompat.getColor(getActivity(), R.color.pink));
+                    enableSave=true;
+                }else{
+                    editBut.setTextColor(ContextCompat.getColor(getActivity(), R.color.cardview_dark_background));
+                    enableSave=false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        //int lengthTitle = edText.getText().toString().trim().length();
         if (mIsAdding)
             editBut.setText("INSERT");
         editBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(enableSave){
                 v.setEnabled(false);
                 HashMap map = item.parseEditItem(insertPointView);
                 if (mIsAdding)
@@ -120,6 +151,9 @@ public class ItemEditFragment extends Fragment {
                         }
                     });
 
+            }else{
+                    Core.makeToast(getResources().getString(R.string.hint_button_title_edit_vendor),false);
+                }
             }
         });
 
